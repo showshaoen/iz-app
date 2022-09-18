291ca101351e6106c5c03e30c5ded0c34b20c192
Shao-En <shaoenshow@gmail.com>
2022-09-18 13:01:48 +0800

    Updated copy to clipboard & tweet daily

--- public/js/calendar/daily.js
+++ public/js/calendar/daily.js
@@ -5,7 +5,6 @@ $(document).ready(async () => {
 
   $("#scheduleForm").on("submit", async (e) => {
     e.preventDefault();
-    console.log(e.target);
     let submitBtn = $(e.target).find("button[name=submitBtn]");
     submitBtn.prop("disabled", true);
     let loader = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...`;
@@ -22,26 +21,59 @@ $(document).ready(async () => {
       {}
     );
 
-    let $body = $("#contentTextArea");
+    let $body = $("#contentDisplay");
     let builder = "";
 
     if (type == "daily") {
       for (let i = 0; i < data.content.length; i++) {
+        let cardBody = "";
         if (data.content.length > 1) {
-          builder += `${data.title} (${i + 1}/${data.content.length})\n`;
+          cardBody += `${data.title} (${i + 1}/${data.content.length})\n`;
         } else {
-          builder += `${data.title}\n`;
+          cardBody += `${data.title}\n`;
         }
-        builder += data.header;
-        builder += `${data.content[i]}\n`;
+        cardBody += data.header;
+        cardBody += `${data.content[i]}`;
+
+        builder += await cardBuilder("Daily Tweet", cardBody);
       }
     } else if (type == "reminder") {
+      console.log(data.content);
       for (x of data.content) {
-        builder += x;
+        builder += await cardBuilder(x.scheduleTime, x.reminder);
+        // builder += x;
       }
+    } else if (type == "tweetDaily") {
+      builder += await cardBuilder("Tweeted succesfully!", data);
     }
 
     $body.html(builder);
+
+    // Clipboard
+    var clipboard = new ClipboardJS(".clipboard", {
+      text: function (trigger) {
+        console.log(trigger.previousSibling.innerHTML);
+        return trigger.previousSibling.innerHTML;
+      },
+    });
+
+    clipboard.on("success", function (e) {
+      let icon = `<i class="fa-regular fa-copy pe-2"></i>`;
+      let copied = icon + "Copied!";
+      $(e.trigger).removeClass("btn-info");
+      $(e.trigger).addClass("btn-secondary");
+      $(e.trigger).html(copied);
+      setTimeout(() => {
+        $(e.trigger).removeClass("btn-secondary");
+        $(e.trigger).addClass("btn-info");
+        $(e.trigger).html(icon + "Copy to clipboard");
+      }, 1000);
+    });
+
+    clipboard.on("error", function (e) {
+      console.log("Failed!");
+    });
+
     submitBtn.prop("disabled", false);
     submitBtn.html("Submit");
     $("#scheduleForm").trigger("reset");
@@ -59,6 +91,25 @@ async function getContent(input) {
       console.log(err);
     },
   });
+}
+
+async function cardBuilder(cardHeader, cardBody) {
+  let cardBodyHTML = cardBody.replace(/(?:\r\n|\r|\n)/g, "<br>");
+  let builder = "";
+  // builder += `<div class="col-lg-6">`
+  builder += `<div class="card mb-3">`;
+  builder += `<div class="card-header">${cardHeader}</div>`;
+  builder += `<div class="card-body">`;
+  builder += cardBodyHTML;
+  builder += `<span hidden>${cardBody}</span>`;
+  builder += `<button type="button" class="btn btn-info mt-2 clipboard">`;
+  builder += `<i class="fa-regular fa-copy pe-2"></i>Copy to clipboard`;
+  builder += `</button>`;
+  builder += `</div>`;
+  // builder += `</div>`;
+  builder += `</div>`;
+
+  return builder;
 }
 
 async function ajaxRequest(method, url, data) {
