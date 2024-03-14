d6a318fec2eeaf1e45bddce88ef092c63a811d1d
showshaoen <shaoenshow@gmail>
2024-03-14 19:59:32 +0800

    Remove authentication for api requests

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ var common = require("../services/common");
 
 // req.isAuthenticated is provided from the auth router
 router.get("/", (req, res) => {
-  console.log(req.oidc.isAuthenticated());
+  // console.log(req.oidc.isAuthenticated());
   res.render("index", {
     title: "Home",
     isAuthenticated: req.oidc.isAuthenticated(),
@@ -21,21 +21,6 @@ router.get("/", (req, res) => {
 router.get("/dashboard", requiresAuth(), async (req, res) => {
   let data = {};
 
-  const { token_type, access_token } = req.oidc.accessToken;
-
-  try {
-    const apiResponse = await axios.get(
-      "http://localhost:5000/schedules/private",
-      {
-        headers: {
-          authorization: `${token_type} ${access_token}`,
-        },
-      }
-    );
-    data = apiResponse.data;
-  } catch (err) {
-    // console.log(err);
-  }
   res.render("dashboard", {
     title: "Dashboard",
     isAuthenticated: req.oidc.isAuthenticated,
@@ -44,20 +29,20 @@ router.get("/dashboard", requiresAuth(), async (req, res) => {
   });
 });
 
-router.get("/getDaily/:type/:date", requiresAuth(), async (req, res) => {
-  const { token_type, access_token } = req.oidc.accessToken;
-  let headers = {
-    authorization: `${token_type} ${access_token}`,
-  };
+// router.get("/getDaily/:type/:date", requiresAuth(), async (req, res) => {
+//   const { token_type, access_token } = req.oidc.accessToken;
+//   let headers = {
+//     authorization: `${token_type} ${access_token}`,
+//   };
 
-  var data = {};
-  data = await common.httpRequest(
-    "GET",
-    `http://localhost:5000/schedules/getDaily/${req.params.type}/${req.params.date}`,
-    headers,
-    {}
-  );
-  return res.status(200).send(data);
-});
+//   var data = {};
+//   data = await common.httpRequest(
+//     "GET",
+//     `http://localhost:5000/schedules/getDaily/${req.params.type}/${req.params.date}`,
+//     headers,
+//     {}
+//   );
+//   return res.status(200).send(data);
+// });
 
 module.exports = router;
